cd1492ff174c32fbe7738374cdebb3655dccdb81
Aiden Onstott <aidenonstott@gmail.com>
2019-08-10 16:33:35 -0700

    Fixed unnecessary imports

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,10 +8,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.subsystems.Cannon;
 import frc.robot.subsystems.Drivetrain;
 
