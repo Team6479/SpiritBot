6502aaef3c12382e2b8c2d515ef8f79e2c4ed709
2stinkysocks <zmehall@gmail.com>
2021-08-17 11:51:29 -0700

    Add smartdashboard for cannon

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -39,6 +39,10 @@ public class RobotContainer {
     configureButtonBindings();
   }
 
+  public Cannon getCannon() {
+    return cannon;
+  }
+
   /**
    * Use this method to define your button->command mappings. Buttons can be created by
    * instantiating a {@link GenericHID} or one of its subclasses ({@link
