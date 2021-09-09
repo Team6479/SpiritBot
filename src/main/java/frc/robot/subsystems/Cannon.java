4b7d1cf0b249ce94e649acbfda747658b4a7412b
2stinkysocks <zmehall@gmail.com>
2021-09-09 14:32:15 -0700

    update imports

--- src/main/java/frc/robot/subsystems/Cannon.java
+++ src/main/java/frc/robot/subsystems/Cannon.java
@@ -4,7 +4,6 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CannonConstants;
